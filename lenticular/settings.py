32fc4fcf9b42a331bcd0133bed252747be30cdc6
Evan Tschuy <evantschuy@gmail.com>
2015-03-12 00:49:32 -0700

    Begin Heroku setup

--- lenticular/settings.py
+++ lenticular/settings.py
@@ -80,5 +80,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
