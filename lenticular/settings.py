f637daedd3231c657c39dd1299e89175b19c5dbe
Evan Tschuy <evantschuy@gmail.com>
2015-03-12 19:30:13 -0700

    Update databse settings

--- lenticular/settings.py
+++ lenticular/settings.py
@@ -61,6 +61,12 @@ WSGI_APPLICATION = 'lenticular.wsgi.application'
 
 import dj_database_url
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
